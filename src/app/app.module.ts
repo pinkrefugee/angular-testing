2eeda8d42a2d047746aeaa3a27679bd70d71bdb5
Aliaksei Palevikou <Aliaksei_Palevikou@epam.com>
2020-04-20 15:46:08 +0300

    add pipe-tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/title-case.pipe.spec.ts
@@ -0,0 +1,13 @@
+import { TitleCasePipe } from './title-case.pipe';
+
+fdescribe('TitleCasePipe', () => {
+  const pipe = new TitleCasePipe();
+
+  it('transforms "abc" to "Abc"', () => {
+    expect(pipe.transform('abc')).toBe('Abc');
+  });
+
+  it('transforms "abc def" to "Abc Def"', () => {
+    expect(pipe.transform('abc def')).toBe('Abc Def');
+  });
+});
